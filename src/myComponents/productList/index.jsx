42bb66d5a1dcad716607f0ae48c0849e32c7d95d
Aakanksha <aakankshamalothia777@gmail.com>
2023-07-14 13:11:07 +0530

    changed image source

--- src/myComponents/productList/index.jsx
+++ src/myComponents/productList/index.jsx
@@ -1,21 +1,20 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
-import Demo from '../../Assets/demoImage.png';
 import { AuthContext } from '../../context/AuthContext';
 
 const ProductList = ({ filteredProduct }) => {
   const { user, setUser } = useContext(AuthContext)
 
   const handleCart = (item) => {
-    
+
     setUser({ ...user, cart: [...user.cart, item] })
   }
   return (
     <div style={{ display: "flex", justifyContent: "space-evenly", alignItems: "center", flexWrap: "wrap" }}>
       {filteredProduct.map((product) => {
-        const { _id, productname, price, type } = product
+        const { _id, productname, price, type, image } = product
         return <div key={_id} style={{ border: "2px solid gray", padding: "15px", margin: "20px" }}>
-          <img src={Demo} alt="categories" />
+          <img width="150" height="150" src={image} alt="categories" style={{ objectFit: "contain" }} />
           <h2><i>{type}</i></h2>
           <h3>Price : {price}</h3>
           <button onClick={() => handleCart(product)}>Add to Cart</button>
